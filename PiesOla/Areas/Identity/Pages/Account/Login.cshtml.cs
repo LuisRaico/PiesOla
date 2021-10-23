dc2f14391b6a6bc0b264b5bbbb3ec91b5f205ac0
Cristhian Rojas Soto <xcristhianx739@gmail.com>
2021-10-23 01:04:46 -0500

    agregando background image

--- PiesOla/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ PiesOla/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -50,7 +50,7 @@ namespace PiesOla.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "¿Recordar usuario?")]
             public bool RememberMe { get; set; }
         }
 
